b6ea8c658a32d8ed9e087a8daac0bdc23d9dac4a
chuchuyei <chuchuyei@users.noreply.github.com>
2016-12-29 13:11:24 +0800

    Create download_history_currency.py
    download from taiwan bank

---
+++ download_history_currency.py
@@ -0,0 +1,47 @@
+import os
+from selenium import webdriver
+from tqdm import tqdm
+import pandas as pd
+import time
+
+path = r'D:\Users\JYJU\Downloads'
+try:
+    filename = [f for f in os.listdir(path) if 'ExchangeRate@' in f][0]
+    os.remove(os.path.join(path, filename))
+except Exception:
+    pass
+
+fp = webdriver.FirefoxProfile()
+fp.set_preference("browser.download.folderList",2)
+fp.set_preference("browser.download.manager.showWhenStarting",False)
+fp.set_preference("browser.download.dir", os.getcwd())
+fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+chromedriver = r'D:\Users\JYJU\tools\chromedriver.exe'
+browser = webdriver.Chrome(chromedriver)
+
+def transfer_file(kind_of_currency):
+    while True:
+        try:
+            filename = [f for f in os.listdir(path) if 'ExchangeRate@' in f and os.path.splitext(f)[-1] == '.csv'][0]
+            break
+        except Exception:
+            time.sleep(0.1)
+    df = pd.read_csv(os.path.join(path, filename))
+    new_path = r'D:\Code_JYJU\data'
+    df.to_csv(os.path.join(new_path, filename[:12] + '_' + str(kind_of_currency) + filename[12:]))
+    os.remove(os.path.join(path, filename))
+
+
+currency = ['USD', 'HKD', 'GBP', 'AUD',
+            'CAD', 'SGD', 'CHF', 'JPY',
+            'ZAR', 'SEK', 'NZD', 'THB',
+            'PHP', 'IDR', 'EUR', 'KRW',
+            'VND', 'MYR', 'CNY',
+            ]
+with tqdm(total = len(currency)) as pbar:
+    for i in currency:
+        url = 'http://rate.bot.com.tw/xrt/flcsv/0/l6m/' + str(i)
+        browser.get(url)
+        transfer_file(i)
+        pbar.update()
+browser.close()
